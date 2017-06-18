1e9d9c0347a5f92a858147dd8b4c7ccc69973506
gustavwennerblom <gustav.wennerblom@gmail.com>
2017-06-18 14:29:44 +0200

    Interim work

--- qbpull.py
+++ qbpull.py
@@ -7,11 +7,20 @@ import CREDENTIALS
 
 logging.basicConfig(level=logging.INFO)
 
+
+def getSystemTime():
+    pass
+
+def getAllSamples():
+    pass
+
 if __name__ == "__main__":
     session = Session()
     session.auth = HTTPBasicAuth(CREDENTIALS.username, CREDENTIALS.password)
     client = Client("https://yf2810.customervoice360.com/service/?handler=soap&wsdl=1",
                     transport=Transport(session=session))
     result = client.service.efs_system_getTime('D, d M Y H:i:s')
+    print(result)
+    result = client.service.survey_samples_getList(1337)
     print(result)
 
