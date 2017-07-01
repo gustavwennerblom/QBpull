50febfb2eb3d66872e80c5d6c2ff0c0e5359e0af
gustavwennerblom <gustav.wennerblom@gmail.com>
2017-07-01 21:59:58 +0200

    Functioning method to get latest result

--- qbpull.py
+++ qbpull.py
@@ -10,6 +10,13 @@ logging.basicConfig(level=logging.INFO)
 
 
 class QBadapter:
+    # When instantiating the class, open a session with Questback with admin credentials
+    def __init__(self):
+        self.session = Session()
+        self.session.auth = HTTPBasicAuth(CREDENTIALS.username, CREDENTIALS.password)
+        self.client = Client("https://yf2810.customervoice360.com/service/?handler=soap&wsdl=1",
+                             transport=Transport(session=self.session))
+        logging.info("New QBadapter created")
 
     # Method to get system time (for testing)
     def get_system_time(self):
@@ -22,8 +29,8 @@ class QBadapter:
         print(result)
         pass
 
-    # Method to get survey results as a CSV
-    def get_results(self):
+    # Returns a CSV-formatted text string given a surveyId (integer)
+    def get_results(self, surveyId):
         # surveyId_type = client.get_type('survey:surveyId')
 
         exportTypes_type = self.client.get_type('ns0:survey_surveyExportDataTypes')
@@ -34,37 +41,60 @@ class QBadapter:
                                           charset="UTF-8",
                                           exportTemplate="COMPLETE_SURVEY")
 
-        result = self.client.service.survey_results_getRawdataCSV(surveyId=1394,
+        result = self.client.service.survey_results_getRawdataCSV(surveyId=surveyId,
                                                                   exportTypes=exportTypes,
                                                                   includeVariables=xsd.SkipValue,
                                                                   dateRange=xsd.SkipValue,
                                                                   sort=xsd.SkipValue,
                                                                   config=config)
-
+        logging.info("Queried for result of survey %s" % surveyId)
+        # The actual data in the response is accessed with the key "return". It's in binary Base64 format
         answers_binary = result["return"]
+
+        # Decode the binary data with utf-8 (as specified in charset above)
         answers_text = answers_binary.decode("utf-8")
 
-        with open("OutputTest_b.txt", 'wb') as f1:
-            f1.write(answers_binary)
+        # Write a copy of the found answers to a csv file for better analysis readability (can be removed later)
+        self.writecopy(answers_text)
 
-        with open("OutputText_t.txt", "w") as f2:
-            f2.write(answers_text)
-
+        # Note that this returns one long string with linefeed escape characters
         return answers_text
 
-    def __init__(self):
-        self.session = Session()
-        self.session.auth = HTTPBasicAuth(CREDENTIALS.username, CREDENTIALS.password)
-        self.client = Client("https://yf2810.customervoice360.com/service/?handler=soap&wsdl=1",
-                             transport=Transport(session=self.session))
+    # Writes a variable to a file (expects text)
+    @staticmethod
+    def writecopy(text):
+        with open("dump.csv", "w") as f:
+            f.write(text)
+        return True
+
+    # Retrieves the latest survey result and returns it as a dict-like object
+    def get_latest_result(self, surveyId):
+        answers = self.get_results(surveyId)
+
+        # Create a dictreader to access values more easily. Splitlines() method required to get fieldheaders attribute
+        # function correctly
+        answers_reader = csv.DictReader(answers.splitlines())
+
+
+        # Iterate through all answers and store the "lfdn" value (index) in 'last_lfdn"
+        for row in answers_reader:
+            last_lfdn = row['lfdn']
+        logging.info("lfdn value of last line found as %s" % last_lfdn)
+
+        # Reset reader (each DictReader can only be used once)
+        answers_reader = csv.DictReader(answers.splitlines())
+
+        # Find the last row and return that dict
+        for row in answers_reader:
+            if row['lfdn'] == last_lfdn:
+                return row
+
+        # In case of failure to find the last row, raise error
+        raise IndexError("Failed to find latest result in method get_latest_result()")
 
 if __name__ == "__main__":
     qba = QBadapter()
-
-    answers = qba.get_results()
-    # dialect = csv.Dialect(delimiter=',')
-    # csv.register_dialect("qbcsv", delimiter=",", doublequote=False)
-    # answers_dict = csv.DictReader(answers, dialect="qbcsv")
-    answers_dict = csv.DictReader(answers, dialect="unix")
-    import code
-    code.interact(local=locals())
+    a = qba.get_latest_result(1394)
+    import pprint
+    pprint.pprint(a)
+    print("End")
