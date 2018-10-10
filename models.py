8cd17c3e94f22314983211bb125ddb7dfa2f4fc3
Gustav Wennerblom <gustav.wennerblom@gmail.com>
2018-10-10 20:54:15 +0200

    Added GBD Advisory table

--- models.py
+++ models.py
@@ -27,27 +27,6 @@ class ConsultingServicesTouchpoint(Base):
     survey_date = Column(DateTime)              # bg_52 / m_cg_exp_survey_date
 
 
-class MISTouchpoint(Base):
-    __tablename__ = 'MISTouchpoint'
-    idx = Column(Integer, primary_key=True)
-    lfdn = Column(Integer, unique=True)         # lfdn
-    project_number = Column(Integer)            # bg_08 / m_cgi_project
-    subproject_number = Column(String(15))      # bg_54 / m_cgi_subproject
-    client_number = Column(Integer)             # bg_12 / m_cgi_customer
-    client_name = Column(String(200))           # bg_13 / m_cgi_customer_t
-    p_spec = Column(Integer)                    # bg_16 / m_cgi_pspec
-    assignment_type = Column(String(20))        # bg_18 / m_cgi_assignmenttyp
-    business_area = Column(String(50))          # bg_21 / m_cgi_bus_unit_t
-    project_cc = Column(Integer)                # bg_26 / m_cgi_costc
-    NKI1 = Column(Integer)                      # NKI1
-    NKI2 = Column(Integer)                      # NKI2
-    TradeKPI = Column(Integer)                  # Trade_KPI
-    ImprovementsText = Column(String(3000))     # Improvements_text
-    agr_prepared = Column(DateTime)             # bg_22 / m_cgi_prepared
-    agr_updated = Column(DateTime)              # bg_23 / m_cgi_updated
-    survey_date = Column(DateTime)              # bg_52 / m_cg_exp_survey_date
-
-
 class BESTouchpoint(Base):
     __tablename__ = 'BESTouchpoint'
     idx = Column(Integer, primary_key=True)
@@ -135,3 +114,43 @@ class BusinessPromotionTouchpoint(Base):
     survey_date = Column(DateTime)          # bg_52
 
 
+class MISTouchpoint(Base):
+    __tablename__ = 'MISTouchpoint'
+    idx = Column(Integer, primary_key=True)
+    lfdn = Column(Integer, unique=True)         # lfdn
+    project_number = Column(Integer)            # bg_08 / m_cgi_project
+    subproject_number = Column(String(15))      # bg_54 / m_cgi_subproject
+    client_number = Column(Integer)             # bg_12 / m_cgi_customer
+    client_name = Column(String(200))           # bg_13 / m_cgi_customer_t
+    p_spec = Column(Integer)                    # bg_16 / m_cgi_pspec
+    assignment_type = Column(String(20))        # bg_18 / m_cgi_assignmenttyp
+    business_area = Column(String(50))          # bg_21 / m_cgi_bus_unit_t
+    project_cc = Column(Integer)                # bg_26 / m_cgi_costc
+    NKI1 = Column(Integer)                      # NKI1
+    NKI2 = Column(Integer)                      # NKI2
+    TradeKPI = Column(Integer)                  # Trade_KPI
+    ImprovementsText = Column(String(3000))     # Improvements_text
+    agr_prepared = Column(DateTime)             # bg_22 / m_cgi_prepared
+    agr_updated = Column(DateTime)              # bg_23 / m_cgi_updated
+    survey_date = Column(DateTime)              # bg_52 / m_cg_exp_survey_date
+
+
+class GBDAdvisoryTouchpoint(Base):
+    __tablename__ = 'GBDAdvisoryTouchpoint'
+    idx = Column(Integer, primary_key=True)
+    lfdn = Column(Integer, unique=True)         # lfdn
+    project_number = Column(Integer)            # bg_08 / m_cgi_project
+    subproject_number = Column(String(15))      # bg_54 / m_cgi_subproject
+    client_number = Column(Integer)             # bg_12 / m_cgi_customer
+    client_name = Column(String(200))           # bg_13 / m_cgi_customer_t
+    p_spec = Column(Integer)                    # bg_16 / m_cgi_pspec
+    assignment_type = Column(String(20))        # bg_18 / m_cgi_assignmenttyp
+    business_area = Column(String(50))          # bg_21 / m_cgi_bus_unit_t
+    project_cc = Column(Integer)                # bg_26 / m_cgi_costc
+    NKI1 = Column(Integer)                      # NKI1
+    NKI2 = Column(Integer)                      # NKI2
+    TradeKPI = Column(Integer)                  # Trade_KPI
+    ImprovementsText = Column(String(3000))     # Improvements_text
+    agr_prepared = Column(DateTime)             # bg_22 / m_cgi_prepared
+    agr_updated = Column(DateTime)              # bg_23 / m_cgi_updated
+    survey_date = Column(DateTime)              # bg_52 / m_cg_exp_survey_date
